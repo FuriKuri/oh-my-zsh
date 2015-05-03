492fbad193a09d3d83216e365f27c269c1357186
Theo Pack <tf.pack@gmail.com>
2015-05-03 12:05:09 +0200

    Merge fork

--- tools/update_fork.sh
+++ tools/update_fork.sh
@@ -3,5 +3,5 @@ printf '\033[0;34m%s\033[0m\n' 'Sync fork'
 git pull
 git fetch upstream
 git checkout master
-git rebase upstream/master
+git merge upstream/master -m "Merge upstream/master to master"
 git push
