6d3c6538c3e3d5b5b462231f5fdd051d7a5851be
Theo Pack <tf.pack@gmail.com>
2015-05-03 12:08:55 +0200

    Use origin master

--- tools/update_fork.sh
+++ tools/update_fork.sh
@@ -1,6 +1,6 @@
 printf '\033[0;34m%s\033[0m\n' 'Sync fork'
 
-git pull
+git pull origin master
 git fetch upstream
 git checkout master
 git merge upstream/master -m "Merge upstream/master to master"
